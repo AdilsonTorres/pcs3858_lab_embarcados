8e8f43f06c32e37e9dd83fc3848a423934a14063
Jhonata <jhonata.antunes@outlook.com>
2018-10-30 07:25:29 -0200

    Update person counter logic

--- movidius/person_detector.py
+++ movidius/person_detector.py
@@ -27,6 +27,15 @@ DISP_MULTIPLIER = DISPLAY_DIMS[0] // PREPROCESS_DIMS[0]
 def update_person_number(number):
 	file_name = datetime.datetime.now().strftime('%d-%m-%Y') + '.txt'
 	file_path = os.path.abspath(os.path.dirname(sys.argv[0])) + '/../'
+        try:
+            with open(file_path + file_name, 'r') as f:
+                people_number = int(f.read())
+                if people_number > number:
+                    return
+        except FileNotFoundError:
+            pass
+        except ValueError:
+            pass
 	with open(file_path + file_name, 'w') as f:
 		f.truncate(0)
 		f.write('{}'.format(number))
